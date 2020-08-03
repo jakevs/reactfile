511f9cb144f3f41bcb17e6cce9ee580c0bc163a2
James Van Slyke <60765773+jakevs@users.noreply.github.com>
2020-08-02 21:38:07 -0500

    adding some text and rendering components to page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React from "react";
 import Header from "./components/Header";
-import Body from "./components/Body";
+// import Body from "./components/Body";
 import Footer from "./components/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Body />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "James Van Slyke",
+      subtitle: "Full-Stack Web Developer",
+      text: "Check out my skills"
+    };
+  }
+  render() {
+    return (
+      <>
+        <Header />
+        <h1>Hello, my name is {this.state.title}!</h1>
+        <h2>I am a {this.state.subtitle}!</h2>
+        <h4>{this.state.text}</h4>
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
