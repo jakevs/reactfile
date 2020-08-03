eb1a74cd920b5af0d5a64da99cbdaf4fde6cb22b
James Van Slyke <60765773+jakevs@users.noreply.github.com>
2020-08-02 19:36:26 -0500

    added components folder and got to render

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
