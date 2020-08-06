0f02f0a2e64a117446ce90e7f5f91663561ce7bb
James Van Slyke <60765773+jakevs@users.noreply.github.com>
2020-08-05 21:37:57 -0500

    adding to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,56 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
 
-export default function Footer() {
+function Copyright() {
   return (
-    <div>
-      <p>JVSIII</p>
-    </div>
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        JVSIII
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
   );
 }
+
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    // marginTop: theme.spacing(8),
+    padding: theme.spacing(6, 0)
+  }
+}));
+
+export default function Footer(props) {
+  const classes = useStyles();
+  const { description, title } = props;
+
+  return (
+    <footer className={classes.footer}>
+      <Container maxWidth="lg">
+        <Typography variant="h6" align="center" gutterBottom>
+          {title}
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="textSecondary"
+          component="p"
+        >
+          {description}
+        </Typography>
+        <Copyright />
+      </Container>
+    </footer>
+  );
+}
+
+Footer.propTypes = {
+  description: PropTypes.string,
+  title: PropTypes.string
+};
