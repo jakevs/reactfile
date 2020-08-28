b07e8e10cf54c22819801b6a40e2b4689bf40c39
James Van Slyke <60765773+jakevs@users.noreply.github.com>
2020-08-27 22:38:51 -0500

    added footer link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,10 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
+      <Link
+        color="inherit"
+        href="https://jakevs.github.io/responsiveportfolio/"
+      >
         JVSIII
       </Link>{" "}
       {new Date().getFullYear()}
