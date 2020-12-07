def34ccc8a202727e954b99519a4919447f1b18f
James Van Slyke <60765773+jakevs@users.noreply.github.com>
2020-12-06 20:17:19 -0600

    possible changes to header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,58 +1,91 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import Typography from "@material-ui/core/Typography";
-import Link from "@material-ui/core/Link";
+// import React from "react";
+// import PropTypes from "prop-types";
+// import { makeStyles } from "@material-ui/core/styles";
+// import Container from "@material-ui/core/Container";
+// import Typography from "@material-ui/core/Typography";
+// import Link from "@material-ui/core/Link";
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link
-        color="inherit"
-        href="https://jakevs.github.io/responsiveportfolio/"
-      >
-        JVSIII
-      </Link>{" "}
-      {new Date().getFullYear()}
-    </Typography>
-  );
-}
+// function Copyright() {
+//   return (
+//     <Typography variant="body2" color="textSecondary" align="center">
+//       {"Copyright © "}
+//       <Link
+//         color="inherit"
+//         href="https://jakevs.github.io/responsiveportfolio/"
+//       >
+//         JVSIII
+//       </Link>{" "}
+//       {new Date().getFullYear()}
+//     </Typography>
+//   );
+// }
 
-const useStyles = makeStyles((theme) => ({
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    // marginTop: theme.spacing(8),
-    padding: theme.spacing(6, 0)
+// const useStyles = makeStyles((theme) => ({
+//   footer: {
+//     backgroundColor: theme.palette.background.paper,
+//     // marginTop: theme.spacing(8),
+//     padding: theme.spacing(6, 0)
+//   }
+// }));
+
+// export default function Footer(props) {
+//   const classes = useStyles();
+//   const { description, title } = props;
+
+//   return (
+//     <footer className={classes.footer}>
+//       <Container maxWidth="lg">
+//         <Typography variant="h6" align="center" gutterBottom>
+//           {title}
+//         </Typography>
+//         <Typography
+//           variant="subtitle1"
+//           align="center"
+//           color="textSecondary"
+//           component="p"
+//         >
+//           {description}
+//         </Typography>
+//         <Copyright />
+//       </Container>
+//     </footer>
+//   );
+// }
+
+// Footer.propTypes = {
+//   description: PropTypes.string,
+//   title: PropTypes.string
+// };
+
+import React, { Component } from "react";
+
+export default class Footer extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <footer>
+        <div className="row">
+          <div className="twelve columns">
+            <ul className="social-links">
+              {resumeData.socialLinks &&
+                resumeData.socialLinks.map((item) => {
+                  return (
+                    <li>
+                      <a href={item.url}>
+                        <i className={item.className} />
+                      </a>
+                    </li>
+                  );
+                })}
+            </ul>
+          </div>
+          <div id="go-top">
+            <a className="smoothscroll" title="Back to Top" href="#home">
+              <i className="icon-up-open" />
+            </a>
+          </div>
+        </div>
+      </footer>
+    );
   }
-}));
-
-export default function Footer(props) {
-  const classes = useStyles();
-  const { description, title } = props;
-
-  return (
-    <footer className={classes.footer}>
-      <Container maxWidth="lg">
-        <Typography variant="h6" align="center" gutterBottom>
-          {title}
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="textSecondary"
-          component="p"
-        >
-          {description}
-        </Typography>
-        <Copyright />
-      </Container>
-    </footer>
-  );
 }
-
-Footer.propTypes = {
-  description: PropTypes.string,
-  title: PropTypes.string
-};
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,118 +1,198 @@
-import React from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import HomeIcon from "@material-ui/icons/Home";
+// import React from "react";
+// import AppBar from "@material-ui/core/AppBar";
+// import Toolbar from "@material-ui/core/Toolbar";
+// import IconButton from "@material-ui/core/IconButton";
+// import Typography from "@material-ui/core/Typography";
+// import { makeStyles } from "@material-ui/core/styles";
+// import HomeIcon from "@material-ui/icons/Home";
 
-import Link from "@material-ui/core/Link";
-import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
+// import Link from "@material-ui/core/Link";
+// import Menu from "@material-ui/core/Menu";
+// import MenuItem from "@material-ui/core/MenuItem";
+// import MoreVertIcon from "@material-ui/icons/MoreVert";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1
-  },
-  menuButton: {
-    color: "white",
-    marginRight: theme.spacing(2)
-  },
-  toolbar: {
-    minHeight: 128,
-    alignItems: "flex-start",
-    paddingTop: theme.spacing(1),
-    paddingBottom: theme.spacing(2),
-    backgroundColor: "#1569C7"
-  },
-  title: {
-    flexGrow: 1,
-    alignSelf: "flex-end",
-    fontFamily: "'Cinzel', serif"
-  }
-}));
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     flexGrow: 1
+//   },
+//   menuButton: {
+//     color: "white",
+//     marginRight: theme.spacing(2)
+//   },
+//   toolbar: {
+//     minHeight: 128,
+//     alignItems: "flex-start",
+//     paddingTop: theme.spacing(1),
+//     paddingBottom: theme.spacing(2),
+//     backgroundColor: "#1569C7"
+//   },
+//   title: {
+//     flexGrow: 1,
+//     alignSelf: "flex-end",
+//     fontFamily: "'Cinzel', serif"
+//   }
+// }));
 
-const options = [
-  "None",
-  "Atria",
-  "Callisto",
-  "Dione",
-  "Ganymede",
-  "Hangouts Call",
-  "Luna",
-  "Oberon",
-  "Phobos",
-  "Pyxis",
-  "Sedna",
-  "Titania",
-  "Triton",
-  "Umbriel"
-];
+// const options = [
+//   "None",
+//   "Atria",
+//   "Callisto",
+//   "Dione",
+//   "Ganymede",
+//   "Hangouts Call",
+//   "Luna",
+//   "Oberon",
+//   "Phobos",
+//   "Pyxis",
+//   "Sedna",
+//   "Titania",
+//   "Triton",
+//   "Umbriel"
+// ];
 
-const ITEM_HEIGHT = 48;
+// const ITEM_HEIGHT = 48;
 
-export default function Header() {
-  const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
+// export default function Header() {
+//   const classes = useStyles();
+//   const [anchorEl, setAnchorEl] = React.useState(null);
+//   const open = Boolean(anchorEl);
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+//   const handleClick = (event) => {
+//     setAnchorEl(event.currentTarget);
+//   };
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar className={classes.toolbar}>
-          <Link
-            color="inherit"
-            href="https://jakevs.github.io/responsiveportfolio/"
-          >
-            <HomeIcon />
-          </Link>{" "}
-          <Typography className={classes.title} variant="h5" noWrap>
-            Hello! My Name is James Van Slyke!
-          </Typography>
-          <div>
-            <IconButton
-              aria-label="more"
-              aria-controls="long-menu"
-              aria-haspopup="true"
-              color="inherit"
-              onClick={handleClick}
-            >
-              <MoreVertIcon color="white" />
-            </IconButton>
-            <Menu
-              id="long-menu"
-              anchorEl={anchorEl}
-              keepMounted
-              open={open}
-              onClose={handleClose}
-              PaperProps={{
-                style: {
-                  maxHeight: ITEM_HEIGHT * 4.5,
-                  width: "20ch"
-                }
-              }}
-            >
-              {options.map((option) => (
-                <MenuItem
-                  key={option}
-                  selected={option === "Pyxis"}
-                  onClick={handleClose}
-                >
-                  {option}
-                </MenuItem>
-              ))}
-            </Menu>
+//   const handleClose = () => {
+//     setAnchorEl(null);
+//   };
+//   return (
+//     <div className={classes.root}>
+//       <AppBar position="static">
+//         <Toolbar className={classes.toolbar}>
+//           <Link
+//             color="inherit"
+//             href="https://jakevs.github.io/responsiveportfolio/"
+//           >
+//             <HomeIcon />
+//           </Link>{" "}
+//           <Typography className={classes.title} variant="h5" noWrap>
+//             Hello! My Name is James Van Slyke!
+//           </Typography>
+//           <div>
+//             <IconButton
+//               aria-label="more"
+//               aria-controls="long-menu"
+//               aria-haspopup="true"
+//               color="inherit"
+//               onClick={handleClick}
+//             >
+//               <MoreVertIcon color="white" />
+//             </IconButton>
+//             <Menu
+//               id="long-menu"
+//               anchorEl={anchorEl}
+//               keepMounted
+//               open={open}
+//               onClose={handleClose}
+//               PaperProps={{
+//                 style: {
+//                   maxHeight: ITEM_HEIGHT * 4.5,
+//                   width: "20ch"
+//                 }
+//               }}
+//             >
+//               {options.map((option) => (
+//                 <MenuItem
+//                   key={option}
+//                   selected={option === "Pyxis"}
+//                   onClick={handleClose}
+//                 >
+//                   {option}
+//                 </MenuItem>
+//               ))}
+//             </Menu>
+//           </div>
+//         </Toolbar>
+//       </AppBar>
+//     </div>
+//   );
+// }
+
+import React, { Component } from "react";
+
+export default class Header extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <React.Fragment>
+        <header id="home">
+          <nav id="nav-wrap">
+            <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+              Show navigation
+            </a>
+            <a className="mobile-btn" href="#" title="Hide navigation">
+              Hide navigation
+            </a>
+            <ul id="nav" className="nav">
+              <li className="current">
+                <a className="smoothscroll" href="#home">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a className="smoothscroll" href="#about">
+                  About
+                </a>
+              </li>
+              <li>
+                <a className="smoothscroll" href="#resume">
+                  Resume
+                </a>
+              </li>
+              <li>
+                <a className="smoothscroll" href="#portfolio">
+                  Works
+                </a>
+              </li>
+              <li>
+                <a className="smoothscroll" href="#testimonials">
+                  Stuff
+                </a>
+              </li>
+              <li>
+                <a className="smoothscroll" href="#contact">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="row banner">
+            <div className="banner-text">
+              <h1 className="responsive-headline">I am {resumeData.name}.</h1>
+              <h3 style={{ color: "#fff", fontFamily: "sans-serif " }}>
+                I am a {resumeData.role}.{resumeData.roleDescription}
+              </h3>
+              <hr />
+              <ul className="social">
+                {resumeData.socialLinks &&
+                  resumeData.socialLinks.map((item) => {
+                    return (
+                      <li key={item.name}>
+                        <a href={item.url} target="">
+                          <i className={item.className}></i>
+                        </a>
+                      </li>
+                    );
+                  })}
+              </ul>
+            </div>
           </div>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+          <p className="scrolldown">
+            <a className="smoothscroll" href="#about">
+              <i className="icon-down-circle"></i>
+            </a>
+          </p>
+        </header>
+      </React.Fragment>
+    );
+  }
 }
